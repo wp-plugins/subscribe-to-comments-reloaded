5e4db093ec855af89635c5ce55c9fb2ef68fc354
coolmann <coolmann@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-14 22:35:03 +0000

    Archiving version 1.5.1
    git-svn-id: https://plugins.svn.wordpress.org/subscribe-to-comments-reloaded@332651 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.5.1/options/panel2.php
@@ -0,0 +1,9 @@
+<?php
+// Avoid direct access to this piece of code
+if (strpos($_SERVER['SCRIPT_FILENAME'], basename(__FILE__))){
+	header('Location: /');
+	exit;
+}
+?>
+
+<p>Work in progress :)</p>
