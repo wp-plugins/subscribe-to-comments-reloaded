dbd4ffaef2dd425c5da501568b918750ea514cab
coolmann <coolmann@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-21 11:34:03 +0000

    Fixed a problem with BRs being introduced by other plugins
    git-svn-id: https://plugins.svn.wordpress.org/subscribe-to-comments-reloaded@413142 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/templates/user.php
+++ trunk/templates/user.php
@@ -43,8 +43,7 @@ if(function_exists('qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage'))
 echo "<p>$message</p>";
 ?>
 
-<form action="<?php echo htmlspecialchars($_SERVER['REQUEST_URI']) ?>" method="post" id="post_list_form" name="post_list_form"
-	onsubmit="if(this.sra[0].checked) return confirm('<?php _e('Please remember: this operation cannot be undone. Are you sure you want to proceed?', 'subscribe-reloaded') ?>')">
+<form action="<?php echo htmlspecialchars($_SERVER['REQUEST_URI']) ?>" method="post" id="post_list_form" name="post_list_form" onsubmit="if(this.sra[0].checked) return confirm('<?php _e('Please remember: this operation cannot be undone. Are you sure you want to proceed?', 'subscribe-reloaded') ?>')">
 <fieldset style="border:0">
 <?php
 	$subscriptions = $subscriptions = $wp_subscribe_reloaded->get_subscriptions('email', 'equals', $email, 'dt', 'DESC');
