8edf4a61952676c67636485dd82beb0c24ba2ee4
coolmann <coolmann@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-26 11:47:20 +0000

    Fixed a minor bug about UTF-8 encoding
    git-svn-id: https://plugins.svn.wordpress.org/subscribe-to-comments-reloaded/trunk@415235 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/author.php
+++ templates/author.php
@@ -41,7 +41,7 @@ if (!empty($_POST['email_list'])){
 			break;
 	}
 }
-$message = html_entity_decode(stripslashes(get_option('subscribe_reloaded_author_text')));
+$message = html_entity_decode(stripslashes(get_option('subscribe_reloaded_author_text')), ENT_COMPAT, 'UTF-8');
 if(function_exists('qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage'))
 	$message = qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage($message);
 echo "<p>$message</p>";
